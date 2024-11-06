58c2d97353e8fbbbcdf54ca6734b77a6262b8621
uksayooj <uksayoojjayarajan@gmail.com>
2024-11-06 20:52:38 +0530

    testing

--- file-service/src/main/java/com/filemanagement/file_command_service/constants/Constants.java
+++ file-service/src/main/java/com/filemanagement/file_command_service/constants/Constants.java
@@ -3,4 +3,5 @@ package com.filemanagement.file_command_service.constants;
 public class Constants {
     public static final String File_Upload_Details ="file_upload_details";
     public static final String File_Delete_Details ="file_delete_details";
+    public static final String File_Delete_Details1 ="file_delete_details1";
 }
